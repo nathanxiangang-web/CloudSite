e0ab27b5ae51e8ce1078ee1cf09311d6ec2d3daf
nathanxiangang-web <nathanxiangang@gmail.com>
2026-09-19 18:04:22 +0800

    refactor(system): export platform settings helpers

---
+++ apps/api/cloudsite/platform/settings/__init__.py
@@ -0,0 +1,11 @@
+from .system_admin import (
+    SYNC_INTERVAL_OPTIONS,
+    read_admin_system_settings,
+    save_admin_system_settings,
+)
+
+__all__ = [
+    "SYNC_INTERVAL_OPTIONS",
+    "read_admin_system_settings",
+    "save_admin_system_settings",
+]
